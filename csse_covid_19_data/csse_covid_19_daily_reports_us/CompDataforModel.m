98360fc8043eafe548930013a03e563d24be91bb
WellsRC <chad.richard.wells@gmail.com>
2020-06-19 17:18:17 -0400

    Update

--- csse_covid_19_data/csse_covid_19_daily_reports_us/CompDataforModel.m
+++ csse_covid_19_data/csse_covid_19_daily_reports_us/CompDataforModel.m
@@ -2,66 +2,51 @@ clear;
 clc
 
 % Load Covid Tracking prioject Data
+
+
 T = readtable('COVIDTrackingProject.csv');
-load('StatesA.mat','S');
-SA=S;
-Date=unique(T.date);
-Date=Date(1:end-1); % Need to truncate the date such that the two merged data sets are the same; COVID Tracking project starts Jan 22 and goes to June 16
+load('StatesA2.mat','S');
+Date=(unique(T.date));
 DateFull=(T.date);
-Tests=zeros(length(S),length(Date));
+CTests=zeros(length(S),length(Date)); % Calculated through positive increase and negative increase
+CCases=zeros(length(S),length(Date)); % Cumulative count of positives
+CDeaths=zeros(length(S),length(Date)); % Cumulative count of deaths
 
 N = T.state;
 
-TT=T.positiveIncrease+T.negativeIncrease;
-for jj=1:length(S)
-indx=strcmp({S{jj}},N);
-    for ii=1:length(Date)
-        ff=find(Date(ii)==DateFull(indx));
-        PT=TT(indx);
-        if(~isempty(ff))
-            Tests(jj,ii)=PT(ff);
-        end
-    end
-end
-
-% Load NY Times Data
-
-load('States.mat','S');
-
-T = readtable('NYTimes_Data_States.csv');
-Date=unique(datenum(T.date));
-Date=Date(2:end); % need to remove the first time point as this data set start Jan 21 (the other starts Jan 22)
-DateFull=datenum(T.date);
-CCases=zeros(length(S),length(Date));
-CDeaths=zeros(length(S),length(Date));
-
-N = T.state;
-
-CC=T.cases;
-DD=T.deaths;
+CC=T.positive;
+DD=T.death;
+TT=T.positive+T.negative;
 for jj=1:length(S)
 indx=strcmp({S{jj}},N);
     for ii=1:length(Date)
         ff=find(Date(ii)==DateFull(indx));
         CT=CC(indx);
         DT=DD(indx);
+        PT=TT(indx);
         if(~isempty(ff))
-            CCases(jj,ii)=CT(ff);
-            CDeaths(jj,ii)=DT(ff);
+            CCases(jj,ii)=max(CT(ff),0);
+            CDeaths(jj,ii)=max(DT(ff),0);
+            CTests(jj,ii)=max(PT(ff),0);
         end
     end
 end
+
 
 %% Calcualte daily
 Cases=zeros(size(CCases));
 Deaths=zeros(size(CDeaths));
 
+Tests=zeros(size(CTests));
+
 Cases(:,1)=CCases(:,1);
 Deaths(:,1)=CDeaths(:,1);
+Tests(:,1)=CTests(:,1);
 
 for ii=2:length(CDeaths(1,:))
     Cases(:,ii)=CCases(:,ii)-CCases(:,ii-1);
     Deaths(:,ii)=CDeaths(:,ii)-CDeaths(:,ii-1); 
+    Tests(:,ii)=CTests(:,ii)-CTests(:,ii-1); 
 end
 
 % Correct for negative numbers
@@ -69,53 +54,34 @@ end
 for ii=1:length(Cases(:,1))
    fneg=find(Cases(ii,:)<0);
    for jj=1:length(fneg)
-       if(fneg(jj)<146)
+       if(fneg(jj)<149)
            Cases(ii,fneg(jj))=round(Cases(ii,fneg(jj)-1)+(Cases(ii,fneg(jj)+1)-Cases(ii,fneg(jj)-1))/2);
        else
-           Cases(ii,fneg(jj))=max(round(pchip([1:145],Cases(ii,[1:145]),146)),0); % use pchip for end point, if negative set to zero
+           Cases(ii,fneg(jj))=max(round(pchip([1:148],Cases(ii,[1:148]),149)),0); % use pchip for end point, if negative set to zero
        end
    end
    fneg=find(Deaths(ii,:)<0);
    for jj=1:length(fneg)
-       if(fneg(jj)<146)
+       if(fneg(jj)<149)
             Deaths(ii,fneg(jj))=round(Deaths(ii,fneg(jj)-1)+(Deaths(ii,fneg(jj)+1)-Deaths(ii,fneg(jj)-1))/2);
        else
-            Deaths(ii,fneg(jj))=max(round(pchip([1:145],Deaths(ii,[1:145]),146)),0); % use pchip for end point, if negative set to zero
+            Deaths(ii,fneg(jj))=max(round(pchip([1:148],Deaths(ii,[1:148]),149)),0); % use pchip for end point, if negative set to zero
        end
    end
    
    fneg=find(Tests(ii,:)<0);
    for jj=1:length(fneg)
-       if(fneg(jj)<146)
+       if(fneg(jj)<149)
             Tests(ii,fneg(jj))=round(Tests(ii,fneg(jj)-1)+(Tests(ii,fneg(jj)+1)-Tests(ii,fneg(jj)-1))/2);
        else
-            Tests(ii,fneg(jj))=max(round(pchip([1:145],Tests(ii,[1:145]),146)),0); % use pchip for end point, if negative set to zero
+            Tests(ii,fneg(jj))=max(round(pchip([1:148],Tests(ii,[1:148]),149)),0); % use pchip for end point, if negative set to zero
        end
    end
 end
 
-%% Filter data
-
-StateRem=[6;7;8;9;10;11;12;16;17;18;21;23;24;25;28;30;31;32;33;35;39;42;44;53];
 
 %% Write data
-csvwrite('DataforModel_Test.csv', Tests(StateRem,[datenum('Janaury 22,2020'):datenum('May 22,2020')]-datenum('Janaury 21,2020'))) 
-csvwrite('DataforModel_Incidence.csv', Cases(StateRem,[datenum('Janaury 22,2020'):datenum('May 22,2020')]-datenum('Janaury 21,2020'))) 
-csvwrite('DataforModel_Death.csv', Deaths(StateRem,[datenum('Janaury 22,2020'):datenum('May 22,2020')]-datenum('Janaury 21,2020'))) 
-xlswrite('StatesinFitting.xlsx', [{S{StateRem}}' {SA{StateRem}}']) 
-
-figure(1);
-for ii=1:24
-    subplot(6,4,ii);plot(Deaths(StateRem(ii),[datenum('Janaury 22,2020'):datenum('May 22,2020')]-datenum('Janaury 21,2020')),'k');
-    box off
-    title(S{StateRem(ii)})
-    ylabel('Deaths');
-end
-
-figure(2);
-for ii=1:24
-    subplot(6,4,ii);plot(Cases(StateRem(ii),[datenum('Janaury 22,2020'):datenum('May 22,2020')]-datenum('Janaury 21,2020')),'k');
-    box off
-    title(S{StateRem(ii)})
-    ylabel('Cases');
-end
+xlswrite('DataforModel_Test.xlsx', flip(Tests(:,[datenum('Janaury 22,2020'):datenum('June 18,2020')]-datenum('Janaury 21,2020'))')) 
+xlswrite('DataforModel_Incidence.xlsx', flip(Cases(:,[datenum('Janaury 22,2020'):datenum('June 18,2020')]-datenum('Janaury 21,2020'))')) 
+xlswrite('DataforModel_Death.xlsx', flip(Deaths(:,[datenum('Janaury 22,2020'):datenum('June 18,2020')]-datenum('Janaury 21,2020'))')) 
+xlswrite('StatesinFitting.xlsx', [{S}]) 
